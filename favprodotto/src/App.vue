7de6e3d424a1a000a59a9b5fef5f1c9888eaadde
edoardo cortinovis <144795409+edoardocortinovis@users.noreply.github.com>
2024-05-01 18:55:59 +0000

    Porgetto

---
+++ favprodotto/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+}
+
+body{
+  background-color: rgb(6, 58, 100);
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: white;
+}
+
+nav a.router-link-exact-active {
+  color: white;  
+}
+</style>
